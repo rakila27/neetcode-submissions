2866d423a10a792dc4fc32aebab187ee4ecdfd41
rakila27 <Rajaram-A@ulster.ac.uk>
2026-05-21 13:18:29 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length)
+            return false;
+
+        char[] sSort = s.ToCharArray();
+        char[] tSort = t.ToCharArray();
+
+        Array.Sort(sSort);
+        Array.Sort(tSort);
+
+        return sSort.SequenceEqual(tSort);
+
+
+    }
+}
